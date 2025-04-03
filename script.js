721f8d27fda55d25ef5d6589d6c7f03cd1055cc5
Kuba Drałus <kubadralus@gmail.com>
2025-04-03 21:21:32 +0200

    1, 2, 3, x, 5, 6, 7, x

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
+
 let clickCount = 0;
 
 const countryInput = document.getElementById('country');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
-
-function handleClick() {
-    clickCount++;
-    clicksInfo.innerText = clickCount;
-}
 
 async function fetchAndFillCountries() {
     try {
@@ -18,7 +14,11 @@ async function fetchAndFillCountries() {
         }
         const data = await response.json();
         const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+        countryInput.innerHTML = countries
+          .map(country => `<option value="${country}">${country}</option>`)
+          .join('');
+        
+        getCountryByIP();
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
@@ -29,7 +29,14 @@ function getCountryByIP() {
         .then(response => response.json())
         .then(data => {
             const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            console.log(country);
+            const select = document.getElementById('country');
+            const option = [...select.options].find(option => option.value === country);
+            if (option) {
+                option.selected = true;
+            }
+
+            getCountryCode(country);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -38,7 +45,6 @@ function getCountryByIP() {
 
 function getCountryCode(countryName) {
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-
     fetch(apiUrl)
     .then(response => {
         if (!response.ok) {
@@ -46,19 +52,45 @@ function getCountryCode(countryName) {
         }
         return response.json();
     })
-    .then(data => {        
+    .then(data => {
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
+        const select = document.getElementById('countryCode');
+        const option = [...select.options].find(option => option.value === countryCode);
+        if (option) {
+            option.selected = true;
+        }
+        console.log(countryCode);
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
     });
 }
 
+function submitForm(event) {
+    event.preventDefault(); // dont refresh website
+    
+    let formData = new FormData(document.getElementById("form"));
+    let jsonData = {};
+    
+    formData.forEach((value, key) => {
+        jsonData[key] = value;
+    });
+    console.log(jsonData);
+}
+
+function handleClick() {
+    clickCount++;
+    clicksInfo.innerText = clickCount;
+}
 
 (() => {
-    // nasłuchiwania na zdarzenie kliknięcia myszką
-    document.addEventListener('click', handleClick);
-
     fetchAndFillCountries();
+    document.addEventListener('click', handleClick);
+    
+    document.getElementById("form").addEventListener("submit", (event) => submitForm(event));
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            submitForm(event);
+        }
+    });
 })()
